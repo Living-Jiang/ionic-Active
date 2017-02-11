5aa06932f7a2c030260988f9dbbb47999e3fb308
Living Jiang <694771790@qq.com>
2017-02-11 15:16:05 +0800

    commit pages

--- www/js/config.js
+++
@@ -1,9 +0,0 @@
-angular.module('appConfig', [])
-
-.config([
-	'$ionicConfigProvider',
-	function($ionicConfigProvider) {
-		$ionicConfigProvider.platform.android.tabs.position("buttom");
-		$ionicConfigProvider.platform.ios.tabs.position("buttom");
-}
-]);
--- www/js/global.js
+++
@@ -1,7 +0,0 @@
-angular.module('appGlobal', [])
-
-.constant('GlobalVariable',{
-	DataList_path:'https://api.douban.com/v2/event/list',
-	Location_path:'https://api.douban.com/v2/loc/list',
-	Detail_path:'https://api.douban.com/v2/event/',
-});
